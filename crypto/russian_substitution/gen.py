3f4debd4728123764390fc7c3cae0718dbc2b417
Ivan Novikov <johnnovikov0@gmail.com>
2019-03-23 01:51:11 +0300

    Created template for task substitution

---
+++ crypto/russian_substitution/gen.py
@@ -0,0 +1,40 @@
+import random
+
+
+class Encryptor(object):
+    def __init__(self):
+        self.substitution = {}
+        self.reverse_substitution = {}
+        self.generate_substitution()
+
+    def generate_substitution(self):
+        russian_alpha_lower = [chr(x) for x in range(ord('а'), ord('а') + 32)] + ['ё', ]
+        russian_alpha_upper = [x.upper() for x in russian_alpha_lower]
+        for alpha in [russian_alpha_lower, russian_alpha_upper]:
+            for letter in alpha:
+                available = set(alpha) - set(self.substitution.values())
+                available = list(available)
+                self.substitution[letter] = random.choice(available)
+
+    def generate_reverse_substitution(self):
+        self.reverse_substitution = {v: k for k, v in self.substitution}
+
+    def check(self):
+        if len(self.substitution) < 1:
+            raise ValueError("Substitution table not generated")
+
+    def encrypt(self, text):
+        self.check()
+        cypher = [self.substitution[x] if x in self.substitution.keys() else x for x in text]
+        return ''.join(cypher)
+
+    def decrypt(self, cypher):
+        cypher = [self.reverse_substitution[x] if x in self.reverse_substitution.keys() else x for x in cypher]
+        return ''.join(cypher)
+
+
+e = Encryptor()
+encrypted = e.encrypt("Я ебал меня сосали")
+decrypted = e.decrypt(encrypted)
+assert encrypted == decrypted
+print(encrypted)
