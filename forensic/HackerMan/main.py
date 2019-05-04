39d2d93a361b99a9e981ef7076beb07ee4f09fe9
Raneddo <Romanhik2000@gmail.com>
2019-05-05 00:23:13 +0300

    Hackerman was updated

--- forensic/HackerMan/main.py
+++ forensic/HackerMan/main.py
@@ -5,9 +5,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    secret = '665248b6b28be8ace6e0edccc400e4ce'
+    secret = 'e9a40c0d861499b1db890399a04185b1'
     if request.args.get('secret') == secret:
-        return "shadowctf{H1st0ry_1nf0Rmat1oN_sh0uld_b3_s3cUr3}"
+        return "shadowctf{H1st0ry_1nf0Rmat1oN_sh0uld_b3_s3cUr3_s3c0nd_v3R5i0n}"
     return "shadowctf{NO_FLAG_FOR_YOU} (It's not flag, really)"
 
 
